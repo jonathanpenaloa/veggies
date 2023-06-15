524db28cf151972305b28310da4ada4bdee073d9
sam <samtessema09@gmail.com>
2023-06-14 22:33:46 -0700

    added error handling

--- server.js
+++ server.js
@@ -16,46 +16,79 @@ app.use(cors())
 
 // GET DATA
 app.get('/fruits', async (req, res) => {
-    let databaseResponse = await Fruit.find();
-    res.send(databaseResponse)
+    try {
+        let databaseResponse = await Fruit.find();
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
 });
 
 app.get('/veggies', async (req, res) => {
-    let databaseResponse = await Veggies.find();
-    res.send(databaseResponse)
+    try {
+        let databaseResponse = await Veggies.find();
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
 });
 
 
 
 // CREATE DATA
 app.post('/fruits', async (req, res) => {
-    console.log(req.body);
-    let databaseResponse = await Fruit.create(req.body);
-    res.send(databaseResponse)
+    try {
+        console.log(req.body);
+        let databaseResponse = await Fruit.create(req.body);
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
 })
 
 app.post('/create_veggies', async (req, res) => {
     console.log(req.body);
-    let databaseResponse = await Veggies.create(req.body);
-    res.send(databaseResponse)
+    try {
+        let databaseResponse = await Veggies.create(req.body);
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
+    
 })
 
 
 // UPDATE DATA
 app.put('/fruits/:idOfFruit/:newName', async (req, res) => {
     // step 1 - get information from request (params, queries, req.body)
-    const idOfFruit = req.params.idOfFruit;
-    const newName= req.params.newName;
-    // step 2 use information to make an update request to collection
-    let databaseResponse = await Fruit.findByIdAndUpdate(idOfFruit, {name: newName})
-    res.send(databaseResponse)
+    try {
+        const idOfFruit = req.params.idOfFruit;
+        const newName= req.params.newName;
+        // step 2 use information to make an update request to collection
+        let databaseResponse = await Fruit.findByIdAndUpdate(idOfFruit, {name: newName})
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
+    
 })
 
 // DELETE DATA
 app.delete('/fruits/:idOfFruit', async (req, res) => {
-    const idOfFruit = req.params.idOfFruit;
-    let databaseResponse = await Fruit.findByIdAndDelete(idOfFruit)
-    res.send(databaseResponse)
+    try {
+        const idOfFruit = req.params.idOfFruit;
+        let databaseResponse = await Fruit.findByIdAndDelete(idOfFruit)
+        res.send(databaseResponse)
+    }
+    catch (err) {
+        res.status(404).send(err.message)
+    }
+    
 })
 
 
